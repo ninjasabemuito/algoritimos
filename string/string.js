426461e266d77f214000aac32a36936edce54f10
ninjasabemuito <162368783+ninjasabemuito@users.noreply.github.com>
2024-04-03 17:23:29 -0300

    Add files via upload

---
+++ string/string.js
@@ -0,0 +1,9 @@
+let nome = "xandeco"
+let idade = 12
+const frase = "Meu nome é " + nome + " e tenho " + idade + " anos "
+console.log(frase)
+
+const nome = "Sou foda"
+const idade = 111
+//console.log('meu nome é ' + nome + ', eu tenho ' + idade + ' anos.')//concatenação
+console.log(`Meu nome é ${nome}, eu tenho ${idade} anos.`)//template string
